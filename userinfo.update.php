104457f91df041593b7ea9e0623c5119a970948d
it2nb <wootichai_k@hotmail.com>
2022-02-01 11:19:08 +0700

    edituserinfo

--- userinfo.update.php
+++ userinfo.update.php
@@ -15,106 +15,106 @@ $n = 0;
 
 $query = 'Update userinfo Set ';
 
-if(isset($_POST['userinfoGender'])) {
+if(isset($request->userinfoGender)) {
     $query .= 'userinfoGender=:userinfoGender'; // Update userinfo Set userinfoGender=:userinfoGender
-    $bind[':userinfoGender'] = $_POST['userinfoGender'];
+    $bind[':userinfoGender'] = @$request->userinfoGender;
     $n = 1;
 }
 
-if(isset($_POST['userinfoPrefix'])) {
+if(isset($request->userinfoPrefix)) {
     if($n == 1) {
         $query .= ', ';
     }
     $query .= 'userinfoPrefix=:userinfoPrefix';
     //Update userinfo Set userinfoGender=:userinfoGender, userinfoPrefix=:userinfoPrefix
     //Update userinfo Set userinfoPrefix=:userinfoPrefix
-    $bind[':userinfoPrefix'] = $_POST['userinfoPrefix'];
+    $bind[':userinfoPrefix'] = @$request->userinfoPrefix;
     $n = 1;
 }
 
-if(isset($_POST['userinfoFullname'])) {
+if(isset($request->userinfoFullname)) {
     if($n == 1) {
         $query .= ', ';
     }
     $query .= 'userinfoFullname=:userinfoFullname';
-    $bind[':userinfoFullname'] = $_POST['userinfoFullname'];
+    $bind[':userinfoFullname'] = $request->userinfoFullname;
     $n = 1;
 }
 
-if(isset($_POST['userinfoBirthday'])) {
+if(isset($request->userinfoBirthday)) {
     if($n == 1) {
         $query .= ', ';
     }
     $query .= 'userinfoBirthday=:userinfoBirthday';
-    $bind[':userinfoBirthday'] = $_POST['userinfoBirthday'];
+    $bind[':userinfoBirthday'] = @$request->userinfoBirthday;
     $n = 1;
 }
 
-if(isset($_POST['userinfoAddress'])) {
+if(isset($request->userinfoAddress)) {
     if($n == 1) {
         $query .= ', ';
     }
     $query .= 'userinfoAddress=:userinfoAddress';
-    $bind[':userinfoAddress'] = $_POST['userinfoAddress'];
+    $bind[':userinfoAddress'] = @$request->userinfoAddress;
     $n = 1;
 }
 
-if(isset($_POST['userinfoPhone'])) {
+if(isset($request->userinfoPhone)) {
     if($n == 1) {
         $query .= ', ';
     }
     $query .= 'userinfoPhone=:userinfoPhone';
-    $bind[':userinfoPhone'] = $_POST['userinfoPhone'];
+    $bind[':userinfoPhone'] = @$request->userinfoPhone;
     $n = 1;
 }
 
-if(isset($_POST['userinfoEmail'])) {
+if(isset($request->userinfoEmail)) {
     if($n == 1) {
         $query .= ', ';
     }
     $query .= 'userinfoEmail=:userinfoEmail';
-    $bind[':userinfoEmail'] = $_POST['userinfoEmail'];
+    $bind[':userinfoEmail'] = @$request->userinfoEmail;
     $n = 1;
 }
 
-if(isset($_POST['userinfoWebsite'])) {
+if(isset($request->userinfoWebsite)) {
     if($n == 1) {
         $query .= ', ';
     }
     $query .= 'userinfoWebsite=:userinfoWebsite';
-    $bind[':userinfoWebsite'] = $_POST['userinfoWebsite'];
+    $bind[':userinfoWebsite'] = @$request->userinfoWebsite;
     $n = 1;
 }
 
-if(isset($_POST['userinfoTalent'])) {
+if(isset($request->userinfoTalent)) {
     if($n == 1) {
         $query .= ', ';
     }
     $query .= 'userinfoTalent=:userinfoTalent';
-    $bind[':userinfoTalent'] = $_POST['userinfoTalent'];
+    $bind[':userinfoTalent'] = @$request->userinfoTalent;
     $n = 1;
 }
 
-if(isset($_POST['userinfoHobby'])) {
+if(isset($request->userinfoHobby)) {
     if($n == 1) {
         $query .= ', ';
     }
     $query .= 'userinfoHobby=:userinfoHobby';
-    $bind[':userinfoHobby'] = $_POST['userinfoHobby'];
+    $bind[':userinfoHobby'] = @$request->userinfoHobby;
     $n = 1;
 }
 
-if(isset($_POST['userinfoGoal'])) {
+if(isset($request->userinfoGoal)) {
     if($n == 1) {
         $query .= ', ';
     }
     $query .= 'userinfoGoal=:userinfoGoal';
-    $bind[':userinfoGoal'] = $_POST['userinfoGoal'];
+    $bind[':userinfoGoal'] = @$request->userinfoGoal;
     $n = 1;
 }
 
 $query .= ' Where userID=:userID';
-$bind[':userID'] = $_POST['userID'];
+$bind[':userID'] = @$request->userID;
 
 $userinfoUpdate = $conn->prepare($query);
 $userinfoUpdate->execute($bind);
@@ -124,7 +124,7 @@ if($userinfoUpdate->rowCount() > 0) {
         'status' => 'Success',
         'msg' => 'แก้ไขข้อมูลสำเร็จ',
         'data' => array(
-            'userID' => $_POST['userID']
+            'userID' => @$request->userID
         )
     );
 } else {
